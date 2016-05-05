a9a2b6aa3053ed45e067fb874dd8c3e423c75369
Okan Binli <279955@ogr.ktu.edu.tr>
2016-05-06 00:01:16 +0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,7 @@
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.5-wily/linux-headers-4.5.0-040500_4.5.0-040500.201603140130_all.deb
+
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.5-wily/linux-headers-4.5.0-040500-generic_4.5.0-040500.201603140130_amd64.deb
+
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.5-wily/linux-image-4.5.0-040500-generic_4.5.0-040500.201603140130_amd64.deb
+
+sudo dpkg -i *.deb
